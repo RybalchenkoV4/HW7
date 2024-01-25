d1fc414b8b0ec9ac17630a021f4822ec330cfbd9
Rybalchenko Vladimir <rybalchenko.v4@gmail.com>
2024-01-25 23:38:25 +0300

    added task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+Random rnd = new Random();
+int [] matrix = new int[10];
+
+void FillingArray()
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        matrix[i] = rnd.Next(0,100);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        Console.Write($"{matrix[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void RevArray(int [] array, int count)
+{
+    if(count < array.Length)
+    {
+        RevArray(array, count+1);
+        Console.Write($"{array[count]} ");
+    }
+}
+
+FillingArray();
+PrintArray();
+RevArray(matrix, 0);
